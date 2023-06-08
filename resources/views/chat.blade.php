63c1d1d5b7147d10dabe663cc10556a31191ed51
Jorane Membrez <jorane.membrez@outlook.com>
2023-06-08 09:14:45 +0200

    CSS chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -82,6 +82,15 @@
     <img id = "imgCover" src="imgSaucisse9.png" alt="Cover Image">
     <p id = "titreEmission" >Saucisse 9</p>
   </div>
+      <audio id="audioPlayer" src="saucisse9.mp3"></audio>
+    <button onclick="playAudio()">Play</button>
+
+    <script>
+      function playAudio() {
+        var audio = document.getElementById("audioPlayer");
+        audio.play();
+      }
+    </script>
   <audio id="audioPlayer" controls></audio>
    <svg id="playPauseButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
     <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
