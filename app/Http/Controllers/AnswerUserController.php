4eccf915d0ffec459eafd6c0b82973850b47775d
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-06-08 16:32:25 +0200

    Avancement routing

--- app/Http/Controllers/AnswerUserController.php
+++ app/Http/Controllers/AnswerUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AnswerUserController extends Controller
+
 {
     public function showForm()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,10 @@
 <?php
 
-use App\Events\Message;
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Http\Request;
-
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ChatsController;
-
-use App\Models\Genre;
-//use App\Models\User;
 use App\Http\Controllers\GenreUserController;
 use App\Http\Controllers\AnswerUserController;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\SurveyController;
-use App\Http\Controllers\sondageController;
-use App\Http\Controllers\sondageSachaController;
-use App\Http\Controllers\VoteController;
-use App\Models\Survey;
-use App\Models\Answer;
-use App\Models\AnswerUser;
-use App\Models\GenreUser;
-
 
 
 /*
@@ -37,43 +19,36 @@ use App\Models\GenreUser;
 */
 
 // Route principale
-Route::get('/', function () {
-    return view('welcome');
-})->name('accueil');
+Route::get('/', function () { return view('welcome'); })->name('accueil');
 
 // Chat
-Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'afficheMessage']);
-Route::post('/send-message', [App\Http\Controllers\ChatsController::class, 'enregistrement']);
+Route::middleware('auth')->group(function () {
 
-// Toute la partie sondage
+    Route::get('registerbis', [GenreUserController::class, 'showForm'])->name('registerbis');
+    Route::post('registerbis', [GenreUserController::class, 'saveGenre']);
+
+    Route::resource("mon-compte", UserController::class);
+
+    Route::get('/sondage', [App\Http\Controllers\sondageController::class, 'afficheSondage']);
+    Route::get('/sondagesacha', [App\Http\Controllers\sondageSachaController::class, 'afficheSondage']);
+
+    Route::get("vote", [AnswerUserController::class, 'showForm'])->name('vote');
+    Route::post("vote", [AnswerUserController::class, 'saveAnswer']);
+
+    Route::get('lastsondage', [SurveyController::class, 'lastSurvey'])->name('lastsondage');
+    Route::post('storevote',  [AnswerUserController::class, 'storevote'])->name('storevote');
+
+    Route::get('/refreshhondage', [App\Http\Controllers\sondageController::class, 'refreshSondage']);
+
+    Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'afficheMessage']);
+    Route::post('/send-message', [App\Http\Controllers\ChatsController::class, 'enregistrement']);
 
 Route::middleware('admin')->group(function () {
     Route::get('creationsondage', [SurveyController::class, 'showForm'])->name('creationsondage');
     Route::post('creationsondage', [SurveyController::class, 'saveSurvey']);
 });
 
-Route::get('/sondage', [App\Http\Controllers\sondageController::class, 'afficheSondage']);
-Route::get('/sondagesacha', [App\Http\Controllers\sondageSachaController::class, 'afficheSondage']);
-
-Route::get("vote", [AnswerUserController::class, 'showForm'])->name('vote');
-Route::post("vote", [AnswerUserController::class, 'saveAnswer']);
-
-Route::get('lastsondage', [SurveyController::class, 'lastSurvey'])->name('lastsondage');
-Route::post('storevote',  [AnswerUserController::class, 'storevote'])->name('storevote');
-
-Route::get('/refreshhondage', [App\Http\Controllers\sondageController::class, 'refreshSondage']);
-
-
-/*
-* GESTION DU COMPTE
-*/
-
-// Accès à la page de gestion du compte
-Route::resource("mon-compte", UserController::class);
-
-// Genre
-Route::get('registerbis', [GenreUserController::class, 'showForm'])->name('registerbis');
-Route::post('registerbis', [GenreUserController::class, 'saveGenre']);
+});
 
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
