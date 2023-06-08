75c5fc361f22061b825bea593ed5ae23191fe669
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2023-06-08 14:56:02 +0200

    ajout du pictogramme de logout et modification CSS

--- resources/views/moncompte.blade.php
+++ resources/views/moncompte.blade.php
@@ -7,13 +7,13 @@
 
 @section('header')
 <h1 class="page-header">Profil</h1>
-<svg xmlns="http://www.w3.org/2000/svg" id="porte" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#767676" class="w-6 h-6" width="35">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-</svg>
+<form method="post" action="{{ url('/logout') }}">
+                      @csrf
+<input type="submit" class="svgSubmit" value="">
+</form>
 @endsection
 
 @section('contenu')
-        <br/>
         <h2>{{ $users->username }}</h2>
         <p>{{ $users->first_name }} {{ $users->last_name }}</p>
         <input type="email" id="email" class="secondaire mailInput" name="email" placeholder="{{ $users->email }}"><br/>
