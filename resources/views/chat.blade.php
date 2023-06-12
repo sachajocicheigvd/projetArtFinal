61774601ac6e31ff1f60d2cc55f28e940d21596d
Jocic Sacha <jocicsacha@mbp-de-jocic.einet.ad.eivd.ch>
2023-06-12 16:36:36 +0200

    chat pour jorane

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -81,7 +81,7 @@
                                 <div class = "boutonPourEnvoyer">
                                 <span class="encadressBoutonEnvoie input-group-btn">
                                     <button type="submit" id="send_message" class="btn btn-primary btn-flat boutonEnvoieMessage">
-                                         <svg id = "boutonEnvoi" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60" fill="currentColor" class="w-1000 h-1000">
+                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60" fill="currentColor" class="w-1000 h-1000">
                                         <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
                                         </svg>
                                     </button>
