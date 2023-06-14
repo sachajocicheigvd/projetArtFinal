880b0d25cff90f663af98faa6c2431927d431bfa
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-06-14 13:47:28 +0200

    JS fonctionnel en brut

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,79 @@
 import "./bootstrap";
-// i want to import the others js files here
-import "./appsondage.js";
-// import "./bootstrap.js";
-import "./chatpopup.js";
-import "./monProfil";
-import "./player.js";
-import "./popup.js";
-import "./sondage.js";
-import "./vote.js";
+// // i want to import the others js files here
+// import "./appsondage.js";
+// // import "./bootstrap.js";
+// import "./chatpopup.js";
+// import "./monProfil";
+// import "./player.js";
+// import "./popup.js";
+// import "./sondage.js";
+// import "./vote.js";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import ChatApp from "./ChatApp.vue";
+
+let appup = null;
+// Écoutez les événements sur la chaîne "popup-channel"
+window.Echo.channel("popup-channel").listen("PopupEvent", () => {
+    if (appup) {
+        appup.unmount("#app"); // Démonter l'application si elle est déjà montée
+    }
+
+    appup = createApp(App);
+    appup.mount("#app"); // Monter l'application
+});
+
+/*
+PARTIE POPUP DANS LE CHAT
+*/
+
+// import ChatApp from "./ChatApp.vue";
+// import "./bootstrap";
+
+let app = null;
+
+const popupChat = async () => {
+    const response = await fetch("/lastsondage");
+    const sondage = await response.json();
+    // mettre dans une variable la date actuelle
+    let app = createApp(ChatApp);
+    let duration = new Date(sondage.duration);
+    let now = new Date();
+
+    if (duration > now) {
+        // app = createApp(ChatApp);
+        app.mount("#chatpopup");
+    }
+    setInterval(function () {
+        now = new Date();
+        if (duration < now) {
+            if (app.mounted) {
+                app.unmount("#chatpopup"); // Démonter l'application si elle est déjà montée
+            }
+        }
+    }, 1000);
+
+    window.Echo.channel("chat-popup").listen("ChatPopup", () => {
+        // console.log("chat-popup");
+
+        if (app) {
+            console.log("a présnentntnrefndasjfdjf");
+            app.unmount("#chatpopup"); // Démonter l'application si elle est déjà montée
+        }
+        console.log("chat-popup");
+        // app = createApp(ChatApp);
+        app.mount("#chatpopup"); // Monter l'application
+    });
+};
+
+if (window.location.pathname == "/chat") {
+    popupChat();
+}
+
+/*
+FIN PARTIE POPUP DANS LE CHAT
+*/
 
 $(document).ready(function () {
     $(document).on("click", "#send_message", function (e) {
